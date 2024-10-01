4e175f32761e8a7770127caa06247af4e48f3984
fillipimangrich <costa.mangrich@gmail.com>
2024-10-01 18:40:13 -0300

    remove handlers

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "types.h"
 #include "handlers.h"
 
@@ -7,6 +8,7 @@
 char *buffer = NULL;
 long file_size;
 int cursor = 0;
+
 State state = 0;
 
 Handler handler[] = {
@@ -15,13 +17,166 @@ Handler handler[] = {
 };
 
 
-void lexical_analyzer() {
-    while (cursor < file_size) {
+Token getNextToken() {
+    Token token;
+    token.lexeme[0] = '\0';
+
+    while (buffer[cursor] != '\0') {
         char c = buffer[cursor];
-        
-        handler[state](&c, &state);
+
+        if (isspace(c)) {
+            cursor++;
+            continue;
+        }
+
+        if (isalpha(c)) {
+            int start = cursor;
+            while (isalnum(buffer[cursor])) cursor++;
+            int length = cursor - start;
+            strncpy(token.lexeme, &buffer[start], length);
+            token.lexeme[length] = '\0';
+
+            if (strcmp(token.lexeme, "def") == 0) token.type = DEF;
+            else if (strcmp(token.lexeme, "return") == 0) token.type = RETURN;
+            else if (strcmp(token.lexeme, "if") == 0) token.type = IF;
+            else if (strcmp(token.lexeme, "else") == 0) token.type = ELSE;
+            else if (strcmp(token.lexeme, "for") == 0) token.type = FOR;
+            else if (strcmp(token.lexeme, "break") == 0) token.type = BREAK;
+            else if (strcmp(token.lexeme, "print") == 0) token.type = PRINT;
+            else if (strcmp(token.lexeme, "read") == 0) token.type = READ;
+            else if (strcmp(token.lexeme, "new") == 0) token.type = NEW;
+            else if (strcmp(token.lexeme, "int") == 0) token.type = INT;
+            else if (strcmp(token.lexeme, "float") == 0) token.type = FLOAT;
+            else if (strcmp(token.lexeme, "string") == 0) token.type = STRING;
+            else token.type = IDENT;
+            return token;
+        }
+
+        if (isdigit(c)) {
+            int start = cursor;
+            while (isdigit(buffer[cursor])) cursor++;
+            if (buffer[cursor] == '.') {
+                cursor++;
+                while (isdigit(buffer[cursor])) cursor++;
+                token.type = FLOAT_CONST;
+            } else {
+                token.type = INT_CONST;
+            }
+            int length = cursor - start;
+            strncpy(token.lexeme, &buffer[start], length);
+            token.lexeme[length] = '\0';
+            return token;
+        }
+
+        switch (c) {
+            case '=':
+                if (buffer[cursor + 1] == '=') {
+                    cursor += 2;
+                    token.type = EQ;
+                    strcpy(token.lexeme, "==");
+                } else {
+                    cursor++;
+                    token.type = ASSIGN;
+                    strcpy(token.lexeme, "=");
+                }
+                return token;
+            case '+':
+                cursor++;
+                token.type = PLUS;
+                strcpy(token.lexeme, "+");
+                return token;
+            case '-':
+                cursor++;
+                token.type = MINUS;
+                strcpy(token.lexeme, "-");
+                return token;
+            case '*':
+                cursor++;
+                token.type = MULT;
+                strcpy(token.lexeme, "*");
+                return token;
+            case '/':
+                cursor++;
+                token.type = DIV;
+                strcpy(token.lexeme, "/");
+                return token;
+            case '%':
+                cursor++;
+                token.type = MOD;
+                strcpy(token.lexeme, "%");
+                return token;
+            case '<':
+                if (buffer[cursor + 1] == '=') {
+                    cursor += 2;
+                    token.type = LEQ;
+                    strcpy(token.lexeme, "<=");
+                } else {
+                    cursor++;
+                    token.type = LT;
+                    strcpy(token.lexeme, "<");
+                }
+                return token;
+            case '>':
+                if (buffer[cursor + 1] == '=') {
+                    cursor += 2;
+                    token.type = GEQ;
+                    strcpy(token.lexeme, ">=");
+                } else {
+                    cursor++;
+                    token.type = GT;
+                    strcpy(token.lexeme, ">");
+                }
+                return token;
+            case '{':
+                cursor++;
+                token.type = LBRACE;
+                strcpy(token.lexeme, "{");
+                return token;
+            case '}':
+                cursor++;
+                token.type = RBRACE;
+                strcpy(token.lexeme, "}");
+                return token;
+            case '(':
+                cursor++;
+                token.type = LPAREN;
+                strcpy(token.lexeme, "(");
+                return token;
+            case ')':
+                cursor++;
+                token.type = RPAREN;
+                strcpy(token.lexeme, ")");
+                return token;
+            case ';':
+                cursor++;
+                token.type = SEMICOLON;
+                strcpy(token.lexeme, ";");
+                return token;
+            case ',':
+                cursor++;
+                token.type = COMMA;
+                strcpy(token.lexeme, ",");
+                return token;
+        }
 
         cursor++;
+        token.type = UNKNOWN;
+        token.lexeme[0] = c;
+        token.lexeme[1] = '\0';
+        return token;
+    }
+
+    token.type = END_OF_FILE;
+    return token;
+}
+
+
+void lexical_analyzer() {
+    Token token;
+    
+    while (cursor < file_size) {
+        token = getNextToken();
+        printf("Token: %s , Type: %s\n", token.lexeme, TokenString[token.type]);
     }
 }
 
@@ -62,5 +217,4 @@ int main(int argc, char **argv)
     
     return 0;
 }
-
 
--- src/types.h
+++ src/types.h
@@ -2,6 +2,65 @@
 #define TYPES_H
 
 typedef enum {
+    DEF, RETURN, IF, ELSE, FOR, BREAK, PRINT, READ, NEW,
+    INT, FLOAT, STRING, IDENT, INT_CONST, FLOAT_CONST, STRING_CONST,
+    ASSIGN, PLUS, MINUS, MULT, DIV, MOD, LT, GT, LEQ, GEQ, EQ, NEQ,
+    LBRACE, RBRACE, LPAREN, RPAREN, LBRACK, RBRACK, SEMICOLON, COMMA,
+    END_OF_FILE, UNKNOWN
+} TokenType;
+
+char* TokenString[] = {
+    "DEF",         // def
+    "RETURN",      // return
+    "IF",          // if
+    "ELSE",        // else
+    "FOR",         // for
+    "BREAK",       // break
+    "PRINT",       // print
+    "READ",        // read
+    "NEW",         // new
+
+    "INT",         // int
+    "FLOAT",       // float
+    "STRING",      // string
+    "IDENT",       // identificador
+    "INT_CONST",   // constante inteira
+    "FLOAT_CONST", // constante float
+    "STRING_CONST",// constante string
+
+    "ASSIGN",      // =
+    "PLUS",        // +
+    "MINUS",       // -
+    "MULT",        // *
+    "DIV",         // /
+    "MOD",         // %
+    "LT",          // <
+    "GT",          // >
+    "LEQ",         // <=
+    "GEQ",         // >=
+    "EQ",          // ==
+    "NEQ",         // !=
+
+    "LBRACE",      // {
+    "RBRACE",      // }
+    "LPAREN",      // (
+    "RPAREN",      // )
+    "LBRACK",      // [
+    "RBRACK",      // ]
+    "SEMICOLON",   // ;
+    "COMMA",       // ,
+
+    "END_OF_FILE", // fim do arquivo
+    "UNKNOWN"      // desconhecido
+};
+
+typedef struct {
+    TokenType type;
+    char lexeme[256];  // máximo de 256 caracteres por token
+} Token;
+
+
+typedef enum {
     E0,
     E1,
     E2,
